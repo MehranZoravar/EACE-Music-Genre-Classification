5a8578d8f422c62823c71084cd6d3025a7919928
Mehran Zoravar <mehranzoravar777@gmail.com>
2025-08-19 20:16:34 -0700

    Adding requirements

--- scripts/save_npz.py
+++ scripts/save_npz.py
@@ -1,1 +1,5 @@
-n
+import numpy as np
+
+softmax_outputs = np.load('./softmax_outputs_cal.npy')
+labels = np.load('./labels_cal.npy')
+np.savez('./output/DeiT_model/DeiT_output_cal.npz', smx=softmax_outputs, labels=labels)
